e83a7f123a983402aac2d831a716da4f4cd1ed5d
Toader, Sebastian <st0ad3r@gmail.com>
2018-04-14 15:06:38 +0200

    Move the body of  to  as  is only invoked at JVM startup. The  is always invoked, also in the case of the agent is loaded afetr JVM startup

--- jmx_prometheus_javaagent/src/main/java/io/prometheus/jmx/JavaAgent.java
+++ jmx_prometheus_javaagent/src/main/java/io/prometheus/jmx/JavaAgent.java
@@ -8,44 +8,48 @@ import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
 import io.prometheus.client.hotspot.DefaultExports;
 
+
 public class JavaAgent {
 
    static HTTPServer server;
 
-   public static void premain(String agentArgument, Instrumentation instrumentation) throws Exception {
-     // Bind to all interfaces by default (this includes IPv6).
-     String host = "0.0.0.0";
 
-     // If we have IPv6 address in square brackets, extract it first and then
-     // remove it from arguments to prevent confusion from too namy colons.
-     Integer indexOfClosingSquareBracket = agentArgument.indexOf("]:");
-     if (indexOfClosingSquareBracket >= 0) {
-       host = agentArgument.substring(0, indexOfClosingSquareBracket + 1);
-       agentArgument = agentArgument.substring(indexOfClosingSquareBracket + 2);
-     }
+  public static void agentmain(String agentArgument, Instrumentation inst) throws Exception {
+    // Bind to all interfaces by default (this includes IPv6).
+    String host = "0.0.0.0";
 
-     String[] args = agentArgument.split(":");
-     if (args.length < 2 || args.length > 3) {
-       System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file>");
-       System.exit(1);
-     }
+    // If we have IPv6 address in square brackets, extract it first and then
+    // remove it from arguments to prevent confusion from too namy colons.
+    Integer indexOfClosingSquareBracket = agentArgument.indexOf("]:");
+    if (indexOfClosingSquareBracket >= 0) {
+      host = agentArgument.substring(0, indexOfClosingSquareBracket + 1);
+      agentArgument = agentArgument.substring(indexOfClosingSquareBracket + 2);
+    }
 
-     int port;
-     String file;
-     InetSocketAddress socket;
+    String[] args = agentArgument.split(":");
+    if (args.length < 2 || args.length > 3) {
+      System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file>");
+      System.exit(1);
+    }
 
-     if (args.length == 3) {
-       port = Integer.parseInt(args[1]);
-       socket = new InetSocketAddress(args[0], port);
-       file = args[2];
-     } else {
-       port = Integer.parseInt(args[0]);
-       socket = new InetSocketAddress(host, port);
-       file = args[1];
-     }
+    int port;
+    String file;
+    InetSocketAddress socket;
 
-     new JmxCollector(new File(file)).register();
-     DefaultExports.initialize();
-     server = new HTTPServer(socket, CollectorRegistry.defaultRegistry, true);
-   }
+    if (args.length == 3) {
+      port = Integer.parseInt(args[1]);
+      socket = new InetSocketAddress(args[0], port);
+      file = args[2];
+    } else {
+      port = Integer.parseInt(args[0]);
+      socket = new InetSocketAddress(host, port);
+      file = args[1];
+    }
+
+    new JmxCollector(new File(file)).register();
+    DefaultExports.initialize();
+    server = new HTTPServer(socket, CollectorRegistry.defaultRegistry, true);
+  }
 }
+
+
